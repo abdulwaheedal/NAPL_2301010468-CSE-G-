1f41c73cd46d702921685ee5f814018db323c925
Faaiz <135386027+abdulwaheedal@users.noreply.github.com>
2026-08-11 12:24:52 +0530

    Add numeric operations for addition, subtraction, etc.
    Implement basic numeric operations in main function.

---
+++ Lab1/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	var int1 = 4 
+	var int2 = 8
+	
+	fmt.Println("Numeric Operations")
+	fmt.Printf("Subtraction = %d\n", int1 - int2)
+	fmt.Printf("Addition = %d\n", int1 + int2)
+	fmt.Printf("Division = %d\n", int1 / int2)
+	fmt.Printf("Multiplication = %d\n", int1 * int2)
+}
